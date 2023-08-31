6917cb2152484ec3a702d463b5afd7e2947a18a8
Gihun Ham <ham@e-mirim.hs.kr>
2023-08-31 15:39:37 +0900

    델리게이터를 활용한 버튼 이벤트핸들러 추가

--- DelegatorButtonPrac31/Form1.Designer.cs
+++ DelegatorButtonPrac31/Form1.Designer.cs
@@ -34,9 +34,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(661, 473);
+            this.ClientSize = new System.Drawing.Size(306, 340);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
